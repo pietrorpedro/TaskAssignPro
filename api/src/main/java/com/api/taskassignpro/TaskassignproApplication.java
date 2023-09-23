b3d088e46f95599ffb44e61371b6ba348d93501d
pietrorpedro <pietrorpedro@gmail.com>
2023-09-23 20:16:30 -0300

    Create Spring Boot API

---
+++ api/src/main/java/com/api/taskassignpro/TaskassignproApplication.java
@@ -0,0 +1,13 @@
+package com.api.taskassignpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskassignproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskassignproApplication.class, args);
+	}
+
+}
