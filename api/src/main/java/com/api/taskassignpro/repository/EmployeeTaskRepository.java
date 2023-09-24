206e870dfab32507fbafd1ddb69a0d0de4a53535
pietrorpedro <pietrorpedro@gmail.com>
2023-09-23 23:46:37 -0300

    Updated Employee Task Repository

--- api/src/main/java/com/api/taskassignpro/repository/EmployeeTaskRepository.java
+++ api/src/main/java/com/api/taskassignpro/repository/EmployeeTaskRepository.java
@@ -3,5 +3,10 @@ package com.api.taskassignpro.repository;
 import com.api.taskassignpro.model.employeeTask.EmployeeTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeTaskRepository extends JpaRepository<EmployeeTask, Long> {
+
+    List<EmployeeTask> findByEmployeeId(Long id);
+    List<EmployeeTask> findByTaskId(Long id);
 }
