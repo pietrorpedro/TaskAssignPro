6ed83db8eb42f2048a66db7e46045299668bbbfe
pietrorpedro <pietrorpedro@gmail.com>
2023-09-23 23:06:02 -0300

    Create Task

--- api/src/main/java/com/api/taskassignpro/model/task/Task.java
+++ api/src/main/java/com/api/taskassignpro/model/task/Task.java
@@ -1,9 +1,13 @@
 package com.api.taskassignpro.model.task;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "task")
+@Getter
+@Setter
 public class Task {
 
     @Id
